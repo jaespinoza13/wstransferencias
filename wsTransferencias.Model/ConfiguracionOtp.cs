fc8d9cdba6174c6bdda6ad51e0be10b861ad980b
Axel Loarte <axel_manuel@outlook.es>
2022-02-04 12:01:47 -0500

    offline pago directo

--- wsTransferencias.Model/ConfiguracionOtp.cs
+++ wsTransferencias.Model/ConfiguracionOtp.cs
@@ -9,5 +9,6 @@ namespace wsTransferencias.Model
 		public int int_longitud_otp { get; set; }
 		public int int_intentos_fallidos_otp { get; set; } 
 		public int int_tiempo_duracion_otp { get; set; }
-		public int int_tiempo_regenerar_otp { get; set; }	}
+		public int int_tiempo_regenerar_otp { get; set; }	
+	}
 }
--- wsTransferencias.Neg/BeneficiariosNeg.cs
+++ wsTransferencias.Neg/BeneficiariosNeg.cs
@@ -47,19 +47,19 @@ namespace wsTransferencias.Neg
                 }
                 else
                 {
-                    //var requiereOtp = Utils.Utils.ValidaRequiereOtp( _settingsApi, req_add_beneficiario, "ADD_BENEFICIARIO" ).Result.codigo.Equals( "1009" );
+                    var requiereOtp = Utils.Utils.ValidaRequiereOtp( _settingsApi, req_add_beneficiario, "ADD_BENEFICIARIO" ).Result.codigo.Equals( "1009" );
 
-                    //if(requiereOtp)
-                    //{
-                    //    res_tran = Utils.Utils.ValidaOtp( _settingsApi, req_add_beneficiario ).Result;
-                    //}
+                    if(requiereOtp)
+                    {
+                        res_tran = Utils.Utils.ValidaOtp( _settingsApi, req_add_beneficiario ).Result;
+                    }
 
-                    //if(res_tran.codigo.Equals( "000" ))
-                    //{
-                    //    res_tran = new BeneficiariosDat( _settingsApi ).add_cuentas_beneficiarios( req_add_beneficiario );
-                    //    respuesta.str_res_codigo = res_tran.codigo;
-                    //    respuesta.str_res_info_adicional = res_tran.diccionario["str_error"].ToString();
-                    //}
+                    if(res_tran.codigo.Equals( "000" ))
+                    {
+                        res_tran = new BeneficiariosDat( _settingsApi ).add_cuentas_beneficiarios( req_add_beneficiario );
+                        respuesta.str_res_codigo = res_tran.codigo;
+                        respuesta.str_res_info_adicional = res_tran.diccionario["str_error"].ToString();
+                    }
 
                     res_tran = new BeneficiariosDat( _settingsApi ).add_cuentas_beneficiarios( req_add_beneficiario );
                     respuesta.str_res_codigo = res_tran.codigo;
@@ -256,20 +256,24 @@ namespace wsTransferencias.Neg
 
                     if(res_banred.codigo.Equals( "000" ) || res_banred.codigo.Equals( "0000" ))
                     {
+                        
                         respuesta.str_nombre = (string)res_banred.cuerpo;
                     }
                     else
                     {
                         if(_settingsApi.pago_directo_pruebas == "1")
                         {
+                            res_banred.codigo = "000";
                             //Solo para pruebas
-                            respuesta.str_nombre = String.Empty;
-                            respuesta.str_res_info_adicional = "No se pudo validar la cuenta/tarjeta. Es probable que no haya respuesta de la institución financiera o que algún dato ingresado en el momento del registro de la cuenta/tarjeta sea incorrecto";
+                            respuesta.str_nombre = "USUARIO MEGONLINE";
+                            respuesta.str_res_info_adicional = "TRANSACCIÓN EXITOSA";
                         }
                         else
                         {
                             //La cuenta no se validó en pago directo
-                            res_banred.codigo = "1012";
+                            //res_banred.codigo = "1012";
+                            res_banred.codigo = "5000";
+                            respuesta.str_res_info_adicional = "No se pudo validar la cuenta/tarjeta. Es probable que no haya respuesta de la institución financiera o que algún dato ingresado en el momento del registro de la cuenta/tarjeta sea incorrecto";
                         }
                     }
                     respuesta.str_res_codigo = res_banred.codigo;
@@ -284,7 +288,6 @@ namespace wsTransferencias.Neg
             }
 
             Utils.ServiceLogs.SaveResponseLogs( respuesta, str_operacion, MethodBase.GetCurrentMethod()!.Name, str_clase );
-            //respuesta.str_res_info_adicional = LoadConfigService.FindErrorCode( respuesta.str_res_codigo ).str_valor_fin;
             return respuesta;
         }
     }
