81a9087e78515b3ab2227ef97ae968b2ed278bbd
Axel Loarte <axel_manuel@outlook.es>
2022-02-04 16:28:09 -0500

    Correccion de validación OTP

--- wsTransferencias.Neg/BeneficiariosNeg.cs
+++ wsTransferencias.Neg/BeneficiariosNeg.cs
@@ -59,14 +59,11 @@ namespace wsTransferencias.Neg
                         res_tran = new BeneficiariosDat( _settingsApi ).add_cuentas_beneficiarios( req_add_beneficiario );
                         respuesta.str_res_codigo = res_tran.codigo;
                         respuesta.str_res_info_adicional = res_tran.diccionario["str_error"].ToString();
+                        res_tran = new BeneficiariosDat( _settingsApi ).add_cuentas_beneficiarios( req_add_beneficiario );
                     }
-
-                    res_tran = new BeneficiariosDat( _settingsApi ).add_cuentas_beneficiarios( req_add_beneficiario );
-                    respuesta.str_res_codigo = res_tran.codigo;
-                    respuesta.str_res_info_adicional = res_tran.diccionario["str_error"].ToString();
+                    
                 }
-
-
+                respuesta.str_res_codigo = res_tran.codigo;
                 Utils.ServiceLogs.SaveResponseLogs( respuesta, str_operacion, MethodBase.GetCurrentMethod()!.Name, str_clase );
                 respuesta.str_res_info_adicional = LoadConfigService.FindErrorCode( respuesta.str_res_codigo ).str_valor_fin;
                 return respuesta;
