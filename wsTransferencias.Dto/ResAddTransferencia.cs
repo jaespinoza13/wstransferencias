68192885b445e5ae6a1e89b9485bd45c9f549052
Accel Loarte <accelloarte@gmail.com>
2022-05-06 16:48:29 -0500

    se agrega el numero de cuenta del beneficiario

--- wsTransferencias.Dto/ResAddTransferencia.cs
+++ wsTransferencias.Dto/ResAddTransferencia.cs
@@ -16,6 +16,7 @@ namespace wsTransferencias.Dto
 			public string str_nombre_ifi_ord{ get; set; }
 			public string str_nombre_benef { get; set; }
 			public string str_num_doc_benef { get; set; }
+			public string str_num_cta_benef { get; set; }
 			public string str_correo_benef { get; set; }
 			public decimal dec_monto_tran { get; set; }
 			public decimal dec_comision { get; set; }
@@ -24,25 +25,26 @@ namespace wsTransferencias.Dto
 			public string str_observacion { get; set; }
 			public int int_enviar_banred { get; set; }
             public int int_id_transaccion { get; set; }
+			
 
 
 
 			//public string str_tipo_prod_benef { get; set; }
 			//public string str_tipo_prod_ordenante { get; set; }
-            //public int int_id_cta_ordenante { get; set; }
-            //public string str_descrip_tipo_cta_ordenante { get; set; }
-            //public string str_descrip_moneda_ordenante { get; set; }
-            //public int int_id_cta_beneficiario { get; set; }
-            //public string str_cta_bce_ifi_benef { get; set; }
-            //public string str_tipo_cta_benef { get; set; }
-            //public string str_desc_tipo_cta_benef { get; set; }
-            //public string str_num_cta_benef { get; set; }
-            //public int int_tipo_benef { get; set; }
-            //public int int_tipo_tarj_cred_benef { get; set; }
-            //public string str_desc_tip_tarj_cre_benef { get; set; }
-            //public string str_alias_cta_benef { get; set; }
-            //public int int_id_comprobar_transfer { get; set; }
+			//public int int_id_cta_ordenante { get; set; }
+			//public string str_descrip_tipo_cta_ordenante { get; set; }
+			//public string str_descrip_moneda_ordenante { get; set; }
+			//public int int_id_cta_beneficiario { get; set; }
+			//public string str_cta_bce_ifi_benef { get; set; }
+			//public string str_tipo_cta_benef { get; set; }
+			//public string str_desc_tipo_cta_benef { get; set; }
 
-        }
+			//public int int_tipo_benef { get; set; }
+			//public int int_tipo_tarj_cred_benef { get; set; }
+			//public string str_desc_tip_tarj_cre_benef { get; set; }
+			//public string str_alias_cta_benef { get; set; }
+			//public int int_id_comprobar_transfer { get; set; }
+
+		}
 	}
 }
