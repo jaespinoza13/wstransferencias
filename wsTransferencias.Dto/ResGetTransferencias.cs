e56325b1c5324cdeb77f23ec23ad2da44b3be501
COOPMEGO\lmorocho <lmorocho@mego.net>
2022-05-12 11:52:56 -0500

    número de registros

--- wsTransferencias.Dto/ResGetTransferencias.cs
+++ wsTransferencias.Dto/ResGetTransferencias.cs
@@ -4,7 +4,7 @@ namespace wsTransferencias.Dto
 {
     public class ResGetTransferencias : ResComun
     {
-        public int int_num_paginas { get; set; }
+        public int int_numero_registros { get; set; }
         public List<Transferencias>? lst_tranferencias { get; set; }
     }
 
