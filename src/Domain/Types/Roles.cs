ea1a176700f71063277d4ca5790766d907fa78be
COOPMEGO\lmorocho <lmorocho@mego.net>
2022-07-26 09:10:10 -0500

    habilitar control de claims

--- src/Domain/Types/Roles.cs
+++ src/Domain/Types/Roles.cs
@@ -5,4 +5,6 @@ public static class Rol
     public const string Socio = "Socio";
     public const string SocioInvitadoInterno = "Socio,InvitadoInterno";
     public const string Usuario = "Usuario";
+    public const string InvitadoInterno = "InvitadoInterno";
+    public const string InvitadoExterno = "InvitadoExterno";
 }
---
+++ src/WebUI/Filters/ClaimControlFilter.cs
@@ -0,0 +1,72 @@
+using Application.Common.ISO20022.Models;
+using Domain.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace WebUI.Filters
+{
+    public class ClaimControlFilter : IAuthorizationFilter
+    {
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!validarClaims(context))
+            {
+                ResException resException = new();
+                resException.str_res_codigo = Convert.ToInt32( HttpStatusCode.Unauthorized ).ToString();
+                resException.str_res_id_servidor = "Sesion Caducada";
+                resException.str_res_estado_transaccion = "ERR";
+                resException.dt_res_fecha_msj_crea = DateTime.Now;
+                resException.str_res_info_adicional = "Tu sesión ha caducado, por favor ingresa nuevamente.";
+
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Result = new ObjectResult( resException );
+            }
+            
+        }
+
+        private bool validarClaims(AuthorizationFilterContext context) {
+            var blValidacion = false;
+            context.HttpContext.Request.EnableBuffering();
+            var user = context.HttpContext.User;
+            var claimRol = user.Claims.FirstOrDefault( c => c.Type == ClaimTypes.Role );
+            var streaming = new StreamReader( context.HttpContext.Request.Body );
+            var requestBody = streaming.ReadToEndAsync().Result;
+            context.HttpContext.Request.Body.Position = 0;
+
+            var header = JsonSerializer.Deserialize<Header>( requestBody )!;
+
+            if (claimRol != null && (
+                claimRol!.Value.Equals( Rol.Socio )|| 
+                claimRol!.Value.Equals( Rol.Usuario ))|| 
+                claimRol!.Value.Equals( Rol.InvitadoExterno )
+                )
+            {
+                var claimEnte = user.Claims.FirstOrDefault( c => c.Type == ClaimTypes.NameIdentifier );
+                var claimCanal = user.Claims.FirstOrDefault( c => c.Type == ClaimTypes.Name );
+
+                blValidacion = (header.str_ente.Equals( claimEnte!.Value ) &&
+                   header.str_nemonico_canal.Equals( claimCanal!.Value )) ? true : false;
+
+
+            }
+
+            else if (claimRol != null && claimRol!.Value.Equals( Rol.InvitadoInterno ))
+            {
+                var claimLogin = user.Claims.FirstOrDefault( c => c.Type == ClaimTypes.NameIdentifier );
+                var claimCanal = user.Claims.FirstOrDefault( c => c.Type == ClaimTypes.Name );
+
+                blValidacion = (header.str_login.Equals( claimLogin!.Value ) &&
+                    header.str_nemonico_canal.Equals( claimCanal!.Value )) ? true : false;
+            }
+           
+
+            return blValidacion;
+        
+        }
+ 
+    }
+}
