7d030d3fe29b5aba5f904c12c01d6902e33d9d35
COOPMEGO\lmorocho <lmorocho@mego.net>
2022-09-08 17:56:05 -0500

    encriptar ids de beneficiario

--- src/Application/Beneficiarios/GetCuentasBeneficiario/ResGetCuentasBeneficiario.cs
+++ src/Application/Beneficiarios/GetCuentasBeneficiario/ResGetCuentasBeneficiario.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Application.Common.Cryptography;
 using Application.Common.ISO20022.Models;
 using Domain.Entities;
 
@@ -8,4 +9,15 @@ namespace Application.Beneficiarios.GetCuentasBeneficiario;
 public class ResGetCuentasBeneficiario : ResComun
 {
     public List<CuentasBeneficiario> lst_cuentas_beneficiario { get; set; } = new();
+    public void EncryptAES(ResGetKeys Key)
+    {
+        lst_cuentas_beneficiario.ForEach( cuenta =>
+        {
+            cuenta.str_id = CryptographyAES.Encrypt( cuenta.str_id!, Key.str_llave_simetrica );
+            cuenta.str_id = "";
+
+        } );
+
+
+    }
 }
