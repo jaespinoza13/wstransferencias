e02f8326591a89a5b85876a8b26d0d32476f18d5
COOPMEGO\lmorocho <lmorocho@mego.net>
2022-12-15 16:43:40 -0500

    correcion validacion de la otp

--- src/Infrastructure/gRPC_Clients/Sybase/TransferenciasExternasDat.cs
+++ src/Infrastructure/gRPC_Clients/Sybase/TransferenciasExternasDat.cs
@@ -48,7 +48,6 @@ namespace Infrastructure.gRPC_Clients.Sybase
                 ds.ListaPEntrada.Add( new ParametroEntrada { StrNameParameter = "@str_observacion", TipoDato = TipoDato.VarChar, ObjValue = req_validar_transferencia.str_observacion } );
 
                 ds.ListaPSalida.Add( new ParametroSalida { StrNameParameter = "@o_enviar_banred", TipoDato = TipoDato.Integer } );
-                ds.ListaPSalida.Add( new ParametroSalida { StrNameParameter = "@o_requiere_otp", TipoDato = TipoDato.Integer } );
                 ds.ListaPSalida.Add( new ParametroSalida { StrNameParameter = "@int_id_solicitud", TipoDato = TipoDato.Integer } );
 
 
@@ -62,14 +61,12 @@ namespace Infrastructure.gRPC_Clients.Sybase
                 var str_codigo = lst_valores.Find( x => x.StrNameParameter == "@int_o_error_cod" )!.ObjValue;
                 var str_error = lst_valores.Find( x => x.StrNameParameter == "@str_o_error" )!.ObjValue.Trim();
                 var str_enviar_banred = lst_valores.Find( x => x.StrNameParameter == "@o_enviar_banred" )!.ObjValue.Trim();
-                var str_requiere_otp = lst_valores.Find( x => x.StrNameParameter == "@o_requiere_otp" )!.ObjValue.Trim();
                 var int_id_solicitud = lst_valores.Find( x => x.StrNameParameter == "@int_id_solicitud" )!.ObjValue.Trim();
 
                 respuesta.codigo = str_codigo.ToString().Trim().PadLeft( 3, '0' );
                 respuesta.cuerpo = Funciones.ObtenerDatos( resultado );
                 respuesta.diccionario.Add( "str_error", str_error.ToString() );
                 respuesta.diccionario.Add( "str_enviar_banred", str_enviar_banred.ToString() );
-                respuesta.diccionario.Add( "str_requiere_otp", str_requiere_otp.ToString() );
                 respuesta.diccionario.Add( "int_id_solicitud", int_id_solicitud );
 
             }
