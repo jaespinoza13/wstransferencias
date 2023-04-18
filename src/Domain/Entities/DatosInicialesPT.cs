1a4deec56aef3066df59bdd37b4d58e7ba199d42
COOPMEGO\lmorocho <lmorocho@mego.net>
2023-04-18 14:52:00 -0500

    deploy

--- src/Domain/Entities/DatosInicialesPT.cs
+++ src/Domain/Entities/DatosInicialesPT.cs
@@ -4,7 +4,7 @@ namespace Domain.Entities
 {
     public class DatosInicialesPT
     {
-       
+        
         public int int_dias_vigencia { get; set; }
         public decimal dcm_monto_min { get; set; }
         public decimal dcm_monto_max { get; set; }
