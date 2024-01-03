db90fd3391683239aeb6950a7d2207f4a7667f58
jacarrion1 <jacarrion@mego.net>
2024-01-03 12:01:18 -0500

    feat: Observabilidad del servicio con OpenTelemetry

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,9 +1,138 @@
 using WebUI.Middleware;
 using static AccesoDatosGrpcAse.Neg.DAL;
 using static AccesoDatosGrpcMongo.Neg.DALMongo;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder( args );
 
+#region Observability
+
+/// <summary>
+/// Observability con OpenTelemetry 
+/// jacarrion1 
+/// 03/Ene/2024
+/// 
+/// 
+/// Paquetes necesarios:
+/// 
+/// OpenTelemetry.Exporter.OpenTelemetryProtocol
+/// OpenTelemetry.Extensions.Hosting
+/// OpenTelemetry.Instrumentation.AspNetCore
+/// OpenTelemetry.Instrumentation.Http
+/// OpenTelemetry.Instrumentation.Runtime
+/// OpenTelemetry.Instrumentation.GrpcNetClient -> descargar con Nuget Package Manager:  PM> NuGet\Install-Package OpenTelemetry.Instrumentation.GrpcNetClient -Version 1.6.0-beta.3
+/// OpenTelemetry.Instrumentation.Process -> descargar con Nuget Package Manager:  PM> NuGet\Install-Package OpenTelemetry.Instrumentation.Process -Version 0.5.0-beta.3
+/// </summary>
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource( resource => resource
+         .AddService( serviceName: "wsTransferencias" ) )
+    .WithTracing( tracing => tracing
+         // trazado de peticiones
+         .AddAspNetCoreInstrumentation( o => {
+             o.EnrichWithHttpRequest = (activity, httpRequest) => {
+                 activity.SetTag( "Date_Request", DateTime.Now );
+                 Dictionary<string, string> dic_headers = httpRequest.Headers.ToDictionary( x => x.Key, x => x.Value.ToString() );
+                 foreach(var header in dic_headers) {
+                     if(string.IsNullOrEmpty( header.Value ) ||
+                     header.Key.ToLower().StartsWith( "authorization" ) ||
+                     header.Key.ToLower() == "clave-secreta" ||
+                     header.Key.ToLower() == "secreto") {
+                         continue;
+                     }
+                     activity.SetTag( header.Key, header.Value );
+                 }
+             };
+             o.EnrichWithHttpResponse = (activity, httpResponse) => {
+                 activity.SetTag( "Date_Response", DateTime.Now );
+                 activity.SetTag( "Content_Length2", httpResponse.ContentLength );
+                 Dictionary<string, string> dic_headers = httpResponse.Headers.ToDictionary( x => x.Key, x => x.Value.ToString() );
+                 foreach(var header in dic_headers) {
+                     if(string.IsNullOrEmpty( header.Value )) {
+                         continue;
+                     }
+                     activity.SetTag( header.Key, header.Value );
+                 }
+             };
+             o.EnrichWithException = (activity, exception) => {
+                 activity.SetTag( "exceptionType", exception.GetType().ToString() );
+                 activity.SetTag( "exceptionMessage", exception.Message );
+                 activity.SetTag( "exceptionStackTrace", exception.StackTrace );
+             };
+         } )
+         .AddGrpcClientInstrumentation( o => {
+             o.EnrichWithHttpRequestMessage = (activity, httpRequest) => {
+                 activity.SetTag( "Date_Request", DateTime.Now );
+                 Dictionary<string, string> dic_headers = httpRequest.Headers.ToDictionary( x => x.Key, x => x.Value.ToString() ) ?? new();
+                 foreach(var header in dic_headers) {
+                     if(string.IsNullOrEmpty( header.Value ) ||
+                     header.Key.ToLower().StartsWith( "authorization" ) ||
+                     header.Key.ToLower() == "clave-secreta" ||
+                     header.Key.ToLower() == "secreto") {
+                         continue;
+                     }
+                     activity.SetTag( header.Key, header.Value );
+                 }
+             };
+             o.EnrichWithHttpResponseMessage = (activity, httpResponse) => {
+                 activity.SetTag( "Date_Response", DateTime.Now );
+                 Dictionary<string, string> dic_headers = httpResponse.Headers.ToDictionary( x => x.Key, x => x.Value.ToString() ) ?? new();
+                 foreach(var header in dic_headers) {
+                     if(string.IsNullOrEmpty( header.Value )) {
+                         continue;
+                     }
+                     activity.SetTag( header.Key, header.Value );
+                 }
+             };
+         } )
+         .AddHttpClientInstrumentation( o => {
+             o.EnrichWithHttpRequestMessage = (activity, httpRequest) => {
+                 activity.SetTag( "Date_Request", DateTime.Now );
+                 Dictionary<string, string> dic_headers = httpRequest.Headers.ToDictionary( x => x.Key, x => x.Value.ToString() ) ?? new();
+                 foreach(var header in dic_headers) {
+                     if(string.IsNullOrEmpty( header.Value ) ||
+                     header.Key.ToLower().StartsWith( "authorization" ) ||
+                     header.Key.ToLower() == "clave-secreta" ||
+                     header.Key.ToLower() == "secreto") {
+                         continue;
+                     }
+                     activity.SetTag( header.Key, header.Value );
+                 }
+             };
+             o.EnrichWithHttpResponseMessage = (activity, httpResponse) => {
+                 activity.SetTag( "Date_Response", DateTime.Now );
+                 Dictionary<string, string> dic_headers = httpResponse.Headers.ToDictionary( x => x.Key, x => x.Value.ToString() ) ?? new();
+                 foreach(var header in dic_headers) {
+                     if(string.IsNullOrEmpty( header.Value )) {
+                         continue;
+                     }
+                     activity.SetTag( header.Key, header.Value );
+                 }
+             };
+             o.EnrichWithException = (activity, exception) => {
+                 activity.SetTag( "exceptionType", exception.GetType().ToString() );
+                 activity.SetTag( "exceptionMessage", exception.Message );
+                 activity.SetTag( "exceptionStackTrace", exception.StackTrace );
+             };
+         } )
+         .AddOtlpExporter( exporter => {
+             //TODO: Colocar la URL de donde esucha OpenTelemetry Collector o recuperar dicha URL de forma Dinamica
+             exporter.Endpoint = new Uri( "http://192.168.55.62:9984" );
+             exporter.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+         } ) ).WithMetrics( metric => metric
+               // Recoleccion de metricas 
+               .AddAspNetCoreInstrumentation()
+               .AddHttpClientInstrumentation()
+               .AddRuntimeInstrumentation()
+               .AddProcessInstrumentation()
+               .AddOtlpExporter( exporter => {
+                   //TODO: Colocar la URL de donde esucha OpenTelemetry Collector o recuperar dicha URL de forma Dinamica
+                   exporter.Endpoint = new Uri( "http://192.168.55.62:9984" );
+                   exporter.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+               } ) );
+
+#endregion
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
@@ -11,26 +140,20 @@ builder.Services.AddWebUIServices( builder.Configuration );
 var grpc = builder.Configuration.GetSection( "ApiSettings:GrpcSettings" );
 var url_sybase = grpc.GetValue<string>( "client_grpc_sybase" );
 var url_mongo = grpc.GetValue<string>( "client_grpc_mongo" );
-builder.Services.AddGrpcClient<DALClient>( o =>
-{
+builder.Services.AddGrpcClient<DALClient>( o => {
     o.Address = new Uri( url_sybase );
-} ).ConfigureChannel( c =>
-{
-    c.HttpHandler = new SocketsHttpHandler
-    {
+} ).ConfigureChannel( c => {
+    c.HttpHandler = new SocketsHttpHandler {
         PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
         KeepAlivePingDelay = TimeSpan.FromSeconds( 20 ),
         KeepAlivePingTimeout = TimeSpan.FromSeconds( 60 ),
         EnableMultipleHttp2Connections = true
     };
 } );
-builder.Services.AddGrpcClient<DALMongoClient>( o =>
-{
+builder.Services.AddGrpcClient<DALMongoClient>( o => {
     o.Address = new Uri( url_mongo );
-} ).ConfigureChannel( c =>
-{
-    c.HttpHandler = new SocketsHttpHandler
-    {
+} ).ConfigureChannel( c => {
+    c.HttpHandler = new SocketsHttpHandler {
         PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
         KeepAlivePingDelay = TimeSpan.FromSeconds( 20 ),
         KeepAlivePingTimeout = TimeSpan.FromSeconds( 60 ),
@@ -40,9 +163,8 @@ builder.Services.AddGrpcClient<DALMongoClient>( o =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    
+if(app.Environment.IsDevelopment()) {
+
 }
 
 app.UseSwagger();
